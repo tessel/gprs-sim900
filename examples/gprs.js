cd59a0c4e34ee4e08b50d11790c7800d87b1117e
Frijol <ifoundthemeaningoflife@gmail.com>
2014-05-19 16:34:15 -0700

    added licensing headers to index and example

--- examples/gprs.js
+++ examples/gprs.js
@@ -1,3 +1,6 @@
+// Any copyright is dedicated to the Public Domain.
+// http://creativecommons.org/publicdomain/zero/1.0/
+
 var tessel = require('tessel');
 var hardware = tessel.port('A');
 
--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// Copyright 2014 Technical Machine, Inc. See the COPYRIGHT
+// file at the top-level directory of this distribution.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 /*
 At the center of Tessel's GPRS Module lies the SIM900, documentation for which, including a full list of commands, can be found at: http://wm.sim.com/producten.aspx?id=1019
 The full AT command manual is here: http://wm.sim.com/downloaden.aspx?id=2986
