31bcb18a76b62090ce4f3213a12b1beb6d7b07b2
Matt Miller <matt@facultycreative.com>
2014-06-21 07:47:10 -0400

    add ability to send ctrl+z to tessel

--- examples/command-line.js
+++ examples/command-line.js
@@ -5,8 +5,16 @@
 Control the GPRS Module from the command line. Useful
 for development and testing. 
 
-Reference document: 
-http://www.simcom.us/act_admin/supportfile/SIM900_HD_V1.01(091226).pdf 
+Reference document:
+http://www.simcom.us/act_admin/supportfile/SIM900_HD_V1.01(091226).pdf
+
+!!! Important Note:
+some commands can't normally be sent in the command line, 
+for example Ctrl+Z which is required to end an SMS input. To get 
+around this, certain inputs trigger these special commands. 
+See the line `if(data === '^Z')` below.
+
+You may need to add more depending on your needs.  
 *********************************************/
 
 var tessel = require('tessel');
@@ -24,6 +32,11 @@ gprs.on('ready', function() {
 process.stdin.resume();
 process.stdin.on('data', function (data) {
   data = String(data).replace(/[\r\n]*$/, '');  //  Removes the line endings
+  // this is a trick to allow ending an input when sending SMS
+  // normally pressing Ctrl+Z on a mac send the process into the background
+  if(data.toLowerCase() === '^z' ) {
+    data = new Buffer([0x1a]); // Ctrl+Z
+  }
   console.log('got command', [data]);
   gprs._txrx(data, 10000, function(err, data) {
     console.log('\nreply:\nerr:\t', err, '\ndata:');
