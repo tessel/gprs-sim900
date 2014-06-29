795b99169306d2170f8dab19a33e245629e151f8
Matt Miller <spmatt@Matts-MacBook-Pro.local>
2014-06-29 11:01:34 -0400

    make changes as per PR feedback. Add support for array of specialCases instead just ctrl+z

--- examples/command-line.js
+++ examples/command-line.js
@@ -3,26 +3,42 @@
 
 /*********************************************
 Control the GPRS Module from the command line. Useful
-for development and testing. 
+for development and testing.
 
-Reference document:
+Some ASCII key combinations can't be sent over the command line but are needed
+to command the SimCom900 module. For example, Ctrl+Z denotes the end of text
+input when sending SMS messages. To get around this we have a `specialCases`
+array. You may need to add more special cases depending on your needs
+
+Reference documents:
 http://www.simcom.us/act_admin/supportfile/SIM900_HD_V1.01(091226).pdf
-
-!!! Important Note:
-some commands are not possible to send in the command line, 
-for example Ctrl+Z which is required to end an SMS input. To get 
-around this, certain inputs trigger these special commands. 
-See the line `if(data === '^z')` below.
-
-You may need to add more depending on your needs.  
+http://www.propox.com/download/docs/SIM900_Application_Note.pdf
 *********************************************/
 
 var tessel = require('tessel');
 var hardware = tessel.port['A'];
 var gprslib = require('../'); // Replace '../' with 'gprs-sim900' in your own code
 
+// support non-typeable ASCII characters
+// you may need to add more depending on your needs
+var specialCases = [{
+    keyPress: '^z',
+    command: new Buffer([0x1a])
+}];
+
+// if data matches any special case key press
+// set data to the special case command
+function checkSpecialCases(data) {
+  specialCases.forEach(function(value) {
+    if(data.toLowerCase() === value.keyPress.toLowerCase()){
+      data = value.command;
+    }
+  });
+  return data;
+}
+
 //  Port, callback
-var gprs = gprslib.use(hardware); 
+var gprs = gprslib.use(hardware);
 gprs.on('ready', function() {
   console.log('GPRS module connected to Tessel.');
   console.log('-------');
@@ -37,11 +53,7 @@ gprs.on('ready', function() {
 process.stdin.resume();
 process.stdin.on('data', function (data) {
   data = String(data).replace(/[\r\n]*$/, '');  //  Removes the line endings
-  // this is a trick to allow ending an input when sending SMS
-  // normally pressing Ctrl+Z on a mac send the process into the background
-  if(data.toLowerCase() === '^z' ) {
-    data = new Buffer([0x1a]); // Ctrl+Z
-  }
+  data = checkSpecialCases(data);
   console.log('got command', [data]);
   gprs._txrx(data, 10000, function(err, data) {
     console.log('\nreply:\nerr:\t', err, '\ndata:');
